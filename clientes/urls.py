ec0f5bcfc072d0b4dc2896a607e88d92d38328cb
Sergi Garcia <sergi@ancoradual.com>
2025-04-30 00:43:41 +0200

    CRUD básico de clientes (Create, Read, Update, Delete)

--- clientes/urls.py
+++ clientes/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('login/', views.procesar_login_cliente, name='procesar_login_cliente'),
     path('dashboard/', views.dashboard_cliente, name='dashboard_cliente'),
+    path('listado/', views.listado_clientes, name='listado_clientes'), 
+    path('crear/', views.crear_cliente, name='crear_cliente'),
+    path('editar/<int:cliente_id>/', views.editar_cliente, name='editar_cliente'),
+    path('eliminar/<int:cliente_id>/', views.eliminar_cliente, name='eliminar_cliente'),
     path('logout/', views.logout_cliente, name='logout_cliente'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Cliente
 from django.contrib import messages
-
+from django.shortcuts import get_object_or_404
 
 def login_cliente(request):
     return render(request, 'clientes/login.html')
@@ -31,6 +31,52 @@ def dashboard_cliente(request):
     cliente = Cliente.objects.get(id=cliente_id)
     return render(request, 'clientes/dashboard.html', {'cliente': cliente})
 
+def listado_clientes(request):
+    cliente_id = request.session.get('cliente_id')
+    if not cliente_id:
+        return redirect('login')
+
+    clientes = Cliente.objects.all()
+    return render(request, 'clientes/listado.html', {'clientes': clientes})
+
+def crear_cliente(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        razon_social = request.POST.get('razon_social')
+        cif = request.POST.get('cif')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        Cliente.objects.create(
+            nombre=nombre,
+            razon_social=razon_social,
+            cif=cif,
+            email=email,
+            password=password
+        )
+        return redirect('listado_clientes')
+
+    return render(request, 'clientes/formulario.html', {'modo': 'crear'})
+
+def editar_cliente(request, cliente_id):
+    cliente = get_object_or_404(Cliente, id=cliente_id)
+
+    if request.method == 'POST':
+        cliente.nombre = request.POST.get('nombre')
+        cliente.razon_social = request.POST.get('razon_social')
+        cliente.cif = request.POST.get('cif')
+        cliente.email = request.POST.get('email')
+        cliente.password = request.POST.get('password')
+        cliente.save()
+        return redirect('listado_clientes')
+
+    return render(request, 'clientes/formulario.html', {'cliente': cliente, 'modo': 'editar'})
+
+
+def eliminar_cliente(request, cliente_id):
+    cliente = get_object_or_404(Cliente, id=cliente_id)
+    cliente.delete()
+    return redirect('listado_clientes')
 
 def logout_cliente(request):
 
